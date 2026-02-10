8fb1e7c175e758e791a49b88c65579dd318e01e9
loganlo729 <loganlo729@gmail.com>
2026-02-10 11:52:47 -0500

    Changes to Lab04

--- lab04/lab04.py
+++ lab04/lab04.py
@@ -24,7 +24,16 @@ def run_console_chat(template_file, agent_name='Agent', **kwargs):
         try:
             response = chat.send(input('You: '))
             # Add code here to check which agent chat should be started
-
+            if 'npc' in response:
+                # Start NPC chat
+                print('Starting npc chat...')
+                npc_template_file = 'lab04\\lab04_npc.json'
+                run_console_chat(template_file=npc_template_file, agent_name='npc')
+            elif 'enemy' in response:
+                # Start Enemy chat
+                print('Starting enemy chat...')
+                enemy_template_file = 'lab04\\lab04_enemy.json'
+                run_console_chat(template_file=enemy_template_file, agent_name='enemy')
 
             # But before here.
         except StopIteration as e:
@@ -32,7 +41,9 @@ def run_console_chat(template_file, agent_name='Agent', **kwargs):
 
 if __name__ ==  '__main__':
     # Add code here to start DM chat
-
-
+    dm_template_file = 'lab04\\lab04_dm.json'
+    print('Starting dm chat...')
+    run_console_chat(template_file=dm_template_file, agent_name='dm',
+                     encounters='a goblin ambush, a riddle bridge, and a dragon lair')
     # But before here.
     pass
